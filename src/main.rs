3e5e0275e4a66b4d0618d70a11a0fc0cccd6459f
LucasCscBks <luke100191@gmail.com>
2022-11-11 18:20:59 -0300

    init project todo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Olá deseja adicionar um novo ToDo?");
+        println!("[sim/nao]");
+        let mut ask: String = input();
+        if ask == "sim" {
+            println!("Digite um novo ToDo abaixo:");
+            let todo: String = input();
+            println!("{} // Adicionado com sucesso!", todo);
+        } else if ask == "nao" {
+            println!("Encerrando programa 3...2...1");
+            break
+        }
+    }
+}
+    
+
+fn input() -> String {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
 }
